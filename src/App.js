c53b1a1af78b2c4ef777213c0a73d65a9b6c791c
LuisDani07 <tics270118@gmail.com>
2022-10-25 09:19:16 -0600

    use efect para traer los datos de la api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import {useEffect} from 'react'
 const API_URL='http://www.omdbapi.com?apikey=272965d2'
 
 function App() {
+    const searchMovies=async(title)=>{
+      const response=await fetch(`${API_URL}&s=${title}`)
+      const data=await response.json();
+      console.log(data.Search)
+    }
+useEffect(()=>{
+ searchMovies('spiderman')
+},[]);
   return (
     <div>App</div>
   )
