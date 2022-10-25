9489f456703a383fe3b01f8ece1e682c6193ecaf
LuisDani07 <tics270118@gmail.com>
2022-10-25 09:23:43 -0600

    tarjeta de una palicula con datos estáticos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import {useEffect} from 'react'
 import './App.css';
 import SearchIcon from './search.svg'
 const API_URL='http://www.omdbapi.com?apikey=272965d2'
+const movie1={
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2012",
+    "imdbID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
 
 function App() {
     const searchMovies=async(title)=>{
@@ -20,6 +27,20 @@ useEffect(()=>{
                         value="superman" 
                         onChange={()=>{}}/>
                        <img src={SearchIcon} alt="search" onClick={()=>{}} />
+             </div>
+             <div className='container'>
+                   <div className='movie'>
+                         <div>
+                              <p>{movie1.Year}</p>
+                         </div>
+                         <div>
+                            <img src={movie1.Poster !=='N/A' ?movie1.Poster: 'https://via.placeholder.com/400'} alt={movie1.Title} />
+                         </div>
+                         <div>
+                            <span>{movie1.Type}</span>
+                            <h3>{movie1.Title}</h3>
+                         </div>
+                   </div>
              </div>
 
     </div>
