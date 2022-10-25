a67293fa1d11cf6a052e7188be5a9fed6cebcd10
LuisDani07 <tics270118@gmail.com>
2022-10-25 13:29:24 -0600

    https para que funcion la api

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import SearchIcon from './search.svg'
 import MovieCard from './MovieCard';
 
-const API_URL='http://www.omdbapi.com?apikey=272965d2'
+const API_URL='https://www.omdbapi.com?apikey=272965d2'
 
 
 
